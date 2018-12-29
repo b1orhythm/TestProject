cee8f12f5bb472c592ca18962a51ffea24fabded
Ilya B <46154982+b1orhythm@users.noreply.github.com>
2018-12-29 16:50:24 +0300

    First commit
    Залил новое задание

---
+++ lesson3/test_lesson3.py
@@ -0,0 +1,135 @@
+import pytest
+import requests
+from requests.auth import HTTPBasicAuth
+import random
+from utils import get_list_books
+from utils import get_book
+from utils import add_book
+from utils import update_book
+from utils import delete_book
+
+"""
+
+def test_check_login():
+    url = host + "/login"
+    
+    # Авторизация с валидными данными
+    result = requests.get(url, auth=HTTPBasicAuth(user, password))
+    assert result.status_code == 200
+
+    # Попытка авторизации без данных
+    result = requests.get(url, auth=HTTPBasicAuth('', ''))
+    assert result.status_code == 401
+
+    # Попытка авторизации без пароля
+    result = requests.get(url, auth=HTTPBasicAuth(user, ''))
+    assert result.status_code == 401
+
+    # Попытка авторизации с некорректным паролем
+    result = requests.get(url, auth=HTTPBasicAuth(user, 'test_Password'))
+    assert result.status_code == 401
+
+    # Попытка авторизации с некорректным логином
+    result = requests.get(url, auth=HTTPBasicAuth('Test_User', password))
+    assert result.status_code == 401
+
+"""
+
+def test_check_list_books():
+
+    add_book({'author': '', 'title': ''})
+    add_book({'author': 'unknown', 'title': 'Second Book'})
+
+    data = get_list_books()
+
+    for book in data:
+        assert 'title' in book
+        assert 'author' in book
+        assert 'id' in book
+
+    assert len(data) >= 2
+
+
+@pytest.mark.parametrize('title', ['', 'Book with title', u'Книга с интересным названием', '$^&%@&(*!_+=', '*(&ADIG!7213'*100])
+@pytest.mark.parametrize('author', ['', 'book Author', u'Автор книги', '$^&%@&(*!_+=', '*(&ADIG!7213'*100])
+def test_add_book(title, author):
+    # Запрашиваем текущее кол-во книг
+    amount = len(get_list_books())
+
+    new_book = {'title': title, 'author': author}
+
+    # Добавляем книгу
+    new_book['id'] = add_book(new_book)['id']
+
+    # Повторно запрашиваем список книг
+    list_b = get_list_books()
+
+    assert len(list_b) > amount
+    assert new_book in list_b
+
+
+def test_open_book():
+    # Получаем список книг
+    list_b = get_list_books()
+
+    # Проверяем, что список книг не пустой
+    if len(list_b) != 0:
+        # Выбираем случайный id книги из списка
+        r_book_id = list_b[random.randint(0, len(list_b)-1)]['id']
+
+        # Получаем данные выбранной книги
+        data = get_book(r_book_id)
+
+        assert 'title' in data, "ERROR: Book title not found!"
+        assert 'author' in data, "ERROR: Book author not found!"
+    else:
+        new_book = {'title': 'Good book', 'author': 'Human'}
+
+        # Создаем новую книгу
+        new_book['id'] = add_book(new_book)['id']
+
+        # Получаем данные созданной книги
+        data = get_book(new_book['id'])
+
+        assert 'title' in data, "ERROR: Book title not found!"
+        assert 'author' in data, "ERROR: Book author not found!"
+
+
+def test_delete_book():
+    new_book = {'title': 'Book for delete', 'author': 'unknown'}
+
+    # Добавляем книгу
+    new_book['id'] = add_book(new_book)['id']
+
+    # Получаем текущее кол-во книг
+    amount = len(get_list_books())
+
+    # Удаляем книгу
+    delete_book(new_book['id'])
+
+    # Повторно получаем список книг
+    list_b = get_list_books()
+
+    assert amount > len(list_b)
+    assert new_book not in list_b, "ERROR: Книга не была удалена и присутствует в списке!"
+
+
+@pytest.mark.parametrize('title', ['', 'Book with title', u'Книга с интересным названием', '$^&%@&(*!_+=', '*(&ADIG!7213'*100])
+@pytest.mark.parametrize('author', ['', 'book Author', u'Автор книги', '$^&%@&(*!_+=', '*(&ADIG!7213'*100])
+def test_update_book(title, author):
+    new_book = {'title': 'Book for update', 'author': 'Human'}
+
+    new_data_book = {'title': title, 'author': author}
+
+    # Добавляем книгу
+    new_book['id'] = add_book(new_book)['id']
+
+    # Обновляем данные в ранее добавленной книге
+    update_book(new_book['id'], new_data_book)
+
+    # Повторно получаем данные текущей книги
+    data = get_book(new_book['id'])
+
+    assert data['title'] == title, "ERROR: Book title not updated!"
+    assert data['author'] == author, "ERROR: Book author not updated!"
+
---
+++ lesson3/utils.py
@@ -0,0 +1,106 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from configparser import ConfigParser
+
+
+config = ConfigParser()
+config.read('file_config.conf')
+
+
+def get_conf_param(selection, parametr, default_value):
+    result = config.get(selection, parametr)
+    return result or default_value
+
+
+user = get_conf_param('DEFAULT', 'user', '')
+password = get_conf_param('DEFAULT', 'password', '')
+host = get_conf_param('DEFAULT', 'host', 'http://127.0.0.1:7000')
+
+
+def get(url, cookies=None, auth_data=None):
+    result = requests.get(url, cookies=cookies, auth=auth_data)
+
+    print('GET request to {0}'.format(url))
+    print('Status code: {0}'.format(result.status_code))
+    print('RESPONSE: {0}'.format(result.text))
+
+    return result
+
+
+def post(url, cookies=None, body=None):
+    result = requests.post(url, cookies=cookies, data=body)
+
+    print('POST request to {0}'.format(url))
+    print('Status code: {0}'.format(result.status_code))
+    print('RESPONSE: {0}'.format(result.text))
+
+    return result
+
+
+def put(url, cookies=None, body=None):
+    result = requests.put(url, cookies=cookies, data=body)
+
+    print('PUT request to {0}'.format(url))
+    print('Status code: {0}'.format(result.status_code))
+    print('RESPONSE: {0}'.format(result.text))
+
+    return result
+
+
+def delete(url, cookies=None):
+    result = requests.delete(url, cookies=cookies)
+
+    print('DELETE request to {0}'.format(url))
+    print('Status code: {0}'.format(result.status_code))
+    print('RESPONSE: {0}'.format(result.text))
+
+    return result
+
+
+def auth():
+    url = '{0}/login'.format(host)
+
+    auth_data = HTTPBasicAuth(user, password)
+
+    response = get(url, auth_data=auth_data)
+    data = response.json()
+
+    return {'my_cookie': data['auth_cookie']}
+
+
+def get_list_books():
+    """ Получаем и возвращаем список книг"""
+
+    result = get('{0}/books'.format(host), cookies=auth())
+
+    return result.json()
+
+
+def get_book(book_id):
+
+    result = get('{0}/books/{1}'.format(host, book_id), cookies=auth())
+
+    return result.json()
+
+
+def add_book(book):
+    """ Добавляем и возращаем книгу"""
+
+    result = post('{0}/add_book'.format(host), cookies=auth(), body=book)
+
+    return result.json()
+
+
+def update_book(book_id, book):
+
+    result = put('{0}/books/{1}'.format(host, book_id), cookies=auth(), body=book)
+
+    return result.json()
+
+
+def delete_book(book_id):
+
+    delete('{0}/books/{1}'.format(host, book_id), cookies=auth())
+
+
+
